64a41d5279464871209b2f235cedc85657a28a59
frogbird <matthewparas2020@u.northwestern.edu>
2019-05-15 10:22:51 -0500

    revamped simulation

--- simul.py
+++ simul.py
@@ -1,5 +1,7 @@
 import numpy as np
 import heapq
+import time
+import matplotlib.pyplot as plt
 
 
 # Constants
@@ -20,18 +22,12 @@ DURATION = 1000 # min
 COST_PER_BED_PER_DAY = 12345 # dollars
 
 
-# hemorrhagic = 15%
-# ischemic = 85%
-
-
-
-
 # set the seed
 np.random.seed(23)
 
 class Patient:
     '''
-    Represents a single entity in the system (i.e. a 'stroke patient')
+    Represents a single entity in the system (i.e. a 'patient')
     Contains a unique ID (essentially used for comparison with other patients)
     Spawn Time - time that the patient was sent away from hospital
     Duration - The processing time they would see at the CSC
@@ -48,44 +44,115 @@ class Patient:
             self.duration = np.random.exponential(ISCHEMIC_RATE)
 
 
-        self.duration = np.random.exponential(CSC_PROCESSING_RATE)
+        # self.duration = np.random.exponential(CSC_PROCESSING_RATE)
         self.completion_time = self.spawn_time + self.duration
 
         # cost
-        self.cost = 0
+        # self.cost = 0
+
+    def update_completion_time(self, duration):
+        self.completion_time += duration
 
 class Event:
     '''
     Parent class for an event
     Takes in the event stuff and just wraps both types of events
     '''
-    def __init__(self, patient, completion_time):
+    def __init__(self, patient, completion_time, event_type, hospital_id):
         self.patient = patient
         self.completion_time = completion_time
+        self.event_type = event_type
+        self.hospital_id = hospital_id
 
     def __lt__(self, other):
         return self.completion_time < other.completion_time
 
-class Arrive(Event):
-    '''
-    Represents the arrival to the CSC
-    '''
-    pass
-
-class Depart(Event):
-    '''
-    Represents the departure from the CSC
-    '''
-    pass
+    def pprint(self):
+        print("{} at time {}, hospital {}".format(self.event_type, self.completion_time, self.hospital_id))
 
 
-class PSC:
-    '''
-    This represents the state of the hospital at 
-    '''
-    pass
 
-class CSCSpawner:
+class Hospital:
+    def __init__(self, pid, number_of_beds, processing_mean, transfer_rate, arrival_rate):
+        self.pid = pid
+        self.number_spawned = 0
+        self.max_beds = number_of_beds
+        self.bed_count = 0
+        self.transfer_rate = transfer_rate
+        self.processing_mean = processing_mean
+        self.arrival_rate = arrival_rate
+        self.rejected_count = 0
+        self.time_stamps = []
+    
+    def process_departure(self, departure_event):
+        self.bed_count -= 1
+        self.time_stamps.append((self.bed_count, departure_event.patient.completion_time))
+        return False
+
+    def pprint(self):
+        print("------------------------")
+        print("Hospital ID: ", self.pid)
+        print("Max Beds: ", self.max_beds)
+        print("Number Rejected: ", self.rejected_count)
+
+    def graph_count(self):
+        y_vals = [x[0] for x in self.time_stamps]
+        x_vals = [x[1] for x in self.time_stamps]
+
+        plt.plot(x_vals, y_vals, 'o')
+        plt.show()
+
+class PSC(Hospital):
+    def process_arrival(self, arrival_event):
+
+        # print(self.bed_count)
+
+        patient = arrival_event.patient
+
+        if self.bed_count < self.max_beds:
+
+            if np.random.uniform() < .13:
+                # transfer immediately to the CSC
+                arrival_event = Event(patient, patient.spawn_time + 0, "Arrival", 0)
+                return arrival_event
+            
+            else:
+                self.bed_count += 1
+                # generate a departure event, for the duration of their visit
+                departure_event = Event(patient, patient.completion_time, "Departure", self.pid)
+                return departure_event
+
+        # print("Rejected")
+        self.rejected_count += 1
+
+        self.time_stamps.append((self.bed_count, patient.completion_time))
+
+        return False
+    
+
+
+class CSC(Hospital):
+    def process_arrival(self, arrival_event):
+
+        patient_obj = arrival_event.patient
+
+        if self.bed_count < self.max_beds:
+
+            self.bed_count += 1
+
+            # print(self.bed_count)
+
+            departure_event = Event(patient_obj, patient_obj.completion_time, "Departure", self.pid)
+
+            return departure_event
+
+        # print("Rejected")
+        self.rejected_count += 1
+        return False
+
+
+
+def arrival_spawner(list_of_hospitals):
     '''
     This is just a star model so the CSC is at the center and it is fed by multiple hospitals
     We model the arrival rates to the hospital as the sum of the exit rates from the PSCs
@@ -95,52 +162,40 @@ class CSCSpawner:
 
     We then build up this queue to pull from later in the hospital
     '''
-    def __init__(self):
-        self.duration = DURATION
-        self.current_time = 0
-        self.number_spawned = 0
-        self.spawning_rate = CSC_ARRIVAL_RATE
-        self.spawning_queue = []
 
-        while self.current_time < self.duration:
-            # print("spawning")
-            self.spawn_patient()
+    spawning_queue = []
+    number_spawned = 0
 
-    def spawn_patient(self):
-        new_patient = Patient(self.number_spawned, self.current_time)
-        self.spawning_queue.append(new_patient)
-        self.current_time += np.random.exponential(1.0 / CSC_ARRIVAL_RATE)
-        self.number_spawned += 1
+    for hospital in list_of_hospitals:
+        if isinstance(hospital, PSC):
+            current_time = 0
+            while current_time < 2*DURATION:
+                new_patient = Patient(number_spawned, current_time)
+                new_event = Event(new_patient, current_time, "Arrival", hospital.pid)
+                spawning_queue.append(new_event)
+                current_time += np.random.exponential(1.0 / hospital.arrival_rate)
+                number_spawned += 1
 
-    def pprint(self):
-        print("Duration: {}".format(self.duration))
-        print("Patients Spawned: {}".format(self.number_spawned))
+    return spawning_queue
 
 
-class Hospital:
-    '''
-    This represents the actual Hospital - the CSC where the beds are limited
-    We have a max beds field (max beds)
-    beds = [list of patients who are being treated]
-    rejected_count - exactly what it sounds like
-    spawner_queue - queue of patients to populate the beds with
-    '''
-    def __init__(self):
-        self.max_beds = NUMBER_OF_BEDS_AT_CSC
-        self.bed_count = 0
-        # self.beds = []
-        self.rejected_count = 0
-        self.spawner_queue = CSCSpawner().spawning_queue
-        self.current_time = 0
-        self.event_queue = [Arrive(x, x.spawn_time) for x in self.spawner_queue]
+def build_hospital_dict(list_of_hospitals):
+    hdict = {}
+    for hospital in list_of_hospitals:
+        hdict[hospital.pid] = hospital
+    return hdict
+
+class Simulation:
+    def __init__(self, hospital_dict, event_queue):
+        self.hospital_dict = hospital_dict
+        self.event_queue = event_queue
         heapq.heapify(self.event_queue)
+        self.current_time = 0
         self.duration = DURATION
 
-        # print(len(self.event_queue))
+    def get_hospital(self, hid):
+        return self.hospital_dict[hid]
 
-    def sort_event_queue(self):
-        self.event_queue = sorted(self.event_queue, key = lambda x: x.completion_time)
-    
     def get_next_event(self):
         '''
         Getter to pop off the queue
@@ -150,90 +205,108 @@ class Hospital:
         else:
             return False
 
+    def process_event(self, event):
+        temp_hospital = self.get_hospital(event.hospital_id)
+        
+        if event.event_type == "Arrival":
+
+            print("Arrived: Patient #{}, at time {}, at hospital {}".format(event.patient.id, 
+            self.current_time, event.hospital_id))
+
+            new_event = temp_hospital.process_arrival(event)
+
+        elif event.event_type == "Departure":
+
+            print("Departure: Patient #{}, at time {}, at hospital {}".format(event.patient.id, 
+            self.current_time, event.hospital_id))
+            
+            new_event = temp_hospital.process_departure(event)
+        
+        if new_event:
+            heapq.heappush(self.event_queue, new_event)
+
+        self.current_time = event.completion_time
+        
+        return
+
     def process_arrival(self, arrival):
         '''
-        Process an arrival to the CSC
+        Process an arrival to any hospital
         If the beds are not full, log the arrival and create a departure event
         add it to the queue
 
         If the beds are full, increment the rejected count and move on to the next event
         '''
-        patient_obj = arrival.patient
-        # print(self.bed_count)
-        if self.bed_count < self.max_beds:
-            print("Arrived: Patient #{}, at time {} with {} available beds".format(patient_obj.id, 
-            self.current_time, self.max_beds - self.bed_count))
-            # self.beds.append(patient_obj)
-            self.bed_count += 1
-            departure_event = Depart(patient_obj, patient_obj.completion_time)
-            heapq.heappush(self.event_queue, departure_event)
-            # heapq.heapify(self.event_queue)
-        else:
-            print("Rejected: Patient #{}, beds full at time {}".format(patient_obj.id, self.current_time))
-            self.rejected_count += 1
+        # patient_obj = arrival.patient
+
+        temp_hospital = self.get_hospital(arrival.hospital_id)
+
+        new_event = temp_hospital.process_arrival(arrival)
+
+        if new_event:
+            heapq.heappush(self.event_queue, new_event)
 
     def process_departure(self, departure):
         '''
-        Process a departure from the CSC
-        Increments the beds
+        2 cases:
+            - process a departure from the PSC in which the patient is being transfered to the CSC
+            - process a departure from the CSC
         '''
-        patient_obj = departure.patient
-        print("Departed: Patient #{}, at time {}".format(patient_obj.id, self.current_time))
-        # self.beds = [x for x in self.beds if x.id != patient_obj.id]
-        self.bed_count -= 1
+        # patient_obj = departure.patient
 
-    def process_events(self):
-        # print(len(self.event_queue))
-        event = self.get_next_event()
+        temp_hospital = self.get_hospital(departure.hospital_id)
 
-        if isinstance(event, Arrive):
-            self.process_arrival(event)
-            # pass
+        new_event = temp_hospital.process_departure(departure)
 
-        elif isinstance(event, Depart):
-            self.process_departure(event)
-            # pass
-        
-        self.current_time = event.completion_time
-        # self.sort_event_queue()
+        if new_event:
+            heapq.heappush(self.event_queue, new_event)
 
     def run_simulation(self):
 
-        number_of_events = 0.0
-        bed_count = 0.0
+        # number_of_events = 0.0
+        # bed_count = 0.0
 
         while self.current_time < self.duration:
             # print(self.current_time)
-            self.process_events()
-            bed_count += self.bed_count
-            number_of_events += 1
+            next_event = self.get_next_event()
 
-        average_bed_count = bed_count / number_of_events
+            self.process_event(next_event)
 
-        print("Average Bed Count : {}".format(average_bed_count))
+            # self.process_events()
+            # bed_count += self.bed_count
+            # number_of_events += 1
 
+        # average_bed_count = bed_count / number_of_events
 
-        # print(self.rejected_count)
-
-hospital = Hospital()
-
-hospital.run_simulation()
-
-
-# for patient in hospital.spawner_queue:
-#     print(patient.completion_time)
-
-
-
-# Arrive(15)
+        # print("Average Bed Count : {}".format(average_bed_count))
 
 
 
 
+if __name__ == "__main__":
+    
+    hospital_list = [
+        PSC(1, 25, 4, .14, 4),
+        PSC(2, 25, 4, .14, 4),
+        PSC(3, 25, 4, .14, 4),
+        PSC(4, 25, 4, .14, 4),
+        PSC(5, 25, 4, .14, 4),
+        CSC(0, 25, 4, .14, 4)
+    ]
+    
+    event_queue = arrival_spawner(hospital_list)
 
+    hospital_dict = build_hospital_dict(hospital_list)
 
+    mySimulation = Simulation(hospital_dict, event_queue)
 
+    mySimulation.run_simulation()
 
+    for hospital in hospital_list:
+        # pass
+        hospital.pprint()
+        # print(hospital.time_stamps)
+        hospital.graph_count()
 
 
 
